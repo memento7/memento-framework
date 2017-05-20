df25ba1e5522dc0f8409f269b7c04f00babe1e48
MaybeS <maytryark@gmail.com>
2017-05-21 04:21:59 +0900

    implement emotion extract close #3

--- cluster/pipeline.py
+++ cluster/pipeline.py
@@ -4,7 +4,7 @@ import json
 from KINCluster import Pipeline
 import numpy as np
 
-from utility import get_similar
+from utility import get_similar, get_emotion, filter_quote, Logging
 from item import myItem as Item
 import connection
 import memento_settings as MS
@@ -30,48 +30,70 @@ class PipelineServer(Pipeline):
                               published_time=row.published_time,
                               reply_count=row.reply_count,
                               href_naver=row.href_naver,
+                              cate=row.cate,
+                              imgs=list(chain(*row.imgs)),
                               comments=list(chain(*row.comments)),
                               quotes=list(chain(*row.quotes))))
         return items
 
     def dress_item(self, ext):
 
-        def push_event(title, date, keywords, rate, items):
+        def push_event(topic, keywords, rate, items, emotions):
 
-            def push_article(eid, title, href, comment_count):
+            def push_article(eid, title, href, comment_count, imgs):
                 payload = {
                     "comment_count": comment_count,
-                    "crawl_target": "NAVERNEWS",
+                    "crawl_target": "네이버 뉴스",
                     "source_url": href,
                     "summary": "",
                     "title": title
                 }
+                if imgs:
+                    payload['image'] = {
+                        "url": imgs[0],
+                        "source_link": href,
+                        "weight": 5
+                    }
                 connection.put_data(MS.SERVER_API + 'events/' + str(eid) + '/articles', payload)
 
             payload = {
-                "date" : date + " 00:00:00",
-                "title" : title,
-                "type" : '연예',
+                "date" : str(topic.published_time)[:10] + " 00:00:00",
+                "title" : topic.title,
+                "type" : topic.cate,
                 "status" : 0,
                 "issue_score" : rate,
-                "emotions" : [],
-                "keywords" : [{"keyword": k, "weight": v} for k, v in keywords],
+                "emotions" : [{
+                    "emotion": emotion[0],
+                    "weight": emotion[1]
+                } for emotion in emotions],
+                "images": [{
+                    "url": img,
+                    "source_url": topic.href_naver,
+                    "weight": 10
+                } for img in topic.imgs],
+                "keywords" : [{
+                    "keyword": k,
+                    "weight": v * 100
+                } for k, v in keywords],
                 "summaries" : []
             }
             res = connection.put_data(MS.SERVER_API + 'events', payload)
-            res = json.loads(res)
+            try:
+                res = json.loads(res)
+            except:
+                Logging.log('<<put event error>>:', res)
+                raise 'Error here why?'
             # print ('response-find-id', res)
             eid = res['id']
 
             for item in items:
-                push_article(eid, item.title, item.href_naver, item.reply_count)
+                push_article(eid, item.title, item.href_naver, item.reply_count, item.imgs)
 
             return eid
 
         def get_memento_rate(items):
             return sum([item.reply_count + 1000 for item in items])
         
-        filter_quote = lambda x: " ".join(["".join(y) for y in x])
         def get_property(item):
             return " ".join([item.title, item.content, filter_quote(item.quotes)])
 
@@ -79,8 +101,9 @@ class PipelineServer(Pipeline):
             return
 
         rate = get_memento_rate(ext.items)
+        emot = get_emotion("\n".join(chain(*list(map(lambda x: x.comments, ext.items)))), [self.keyword])
         simi = get_similar(self.keyword, list(map(get_property, ext.items)))
+        simi = [(self.keyword in item.title and v + 0.03 or v) for v, item in zip(simi, ext.items)]
         topic = ext.items[np.argmax(simi)]
-        date = str(topic.published_time)[:10]
         
-        self.eid.append(push_event(topic.title, date, ext.keywords, rate, ext.items))
+        self.eid.append(push_event(topic, ext.keywords, rate, ext.items, emot))
--- cluster/utility.py
+++ cluster/utility.py
@@ -1,9 +1,14 @@
 import logging
+from collections import Counter
+from itertools import chain
 
+from imp import reload
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from konlpy.tag import Komoran
 
 class Logging:
+    reload(logging)
     __log = logging
     __log.basicConfig(format='%(levelname)s:%(message)s',
                       filename='cluster.log',
@@ -18,11 +23,13 @@ class Logging:
 
     def __call__(self, *args, **kwargs):
         Logging.__log.info('Logging: {} with args: {}, kwargs: {}'.format(
-            self.func.__name__,
-            args,
-            kwargs,
+            self.func.__name__, args, kwargs,
         ))
-        return self.func(*args, **kwargs)
+        ret = self.func(*args, **kwargs)
+        Logging.__log.info('Logging: {} returns {}'.format(
+            self.func.__name__, type(ret),
+        ))
+        return ret
 
     @staticmethod
     def log(msg, level='INFO'):
@@ -39,3 +46,10 @@ def get_similar(keyword, items):
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix_train = tfidf_vectorizer.fit_transform([entity] + items)
     return cosine_similarity(tfidf_matrix_train[0:1], tfidf_matrix_train)[0][1:]
+
+TAGGER = Komoran()
+def get_emotion(text, entities, size=5):
+    counter = Counter([(word, tag) for word, tag in TAGGER.pos(text)]).most_common()
+    keywords = list(map(lambda x: (x[0][0], x[1]), filter(lambda x: x[0][1] == 'XR', counter)))
+    tags = [ENTITIES[entity]['tag'] for entity in entities]
+    return list(filter(lambda x: x[0] not in chain(*tags), keywords))[:size]
